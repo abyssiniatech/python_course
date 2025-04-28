317f91ddd1eeb85fab86ecf9625e7a7e75326c2b
surafelmengist <surafelmengist2015@gmail.com>
2025-04-28 19:29:28 +0300

    the intial commit

--- Hello-world.py
+++ Hello-world.py
@@ -1,1 +1,5 @@
 print("Hello these is Python and django ccourse")
+name="surafel mengist"
+age=22
+Address="Addis Ababa"
+print('my name is',name  , "and i 'm ",age,"years old", "and i live in",Address);
