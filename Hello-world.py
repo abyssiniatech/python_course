8ddfbf60aaed50d49aaa85bf0f7cd0a39c06d1c1
surafelmengist <surafelmengist2015@gmail.com>
2025-04-30 19:00:50 +0300

    the intial commit

--- Hello-world.py
+++ Hello-world.py
@@ -1,5 +1,34 @@
-print("Hello these is Python and django ccourse")
-name="surafel mengist"
-age=22
-Address="Addis Ababa"
-print('my name is',name  , "and i 'm ",age,"years old", "and i live in",Address);
+# def  surafel():
+#     print("these is the funtion of surafel")
+# surafel()
+
+# variable in python
+# name="surafel";
+# age=20;
+
+
+# print('my name is',name,'and my age is ',age ,"years old")
+
+
+# if statment in python
+# age=195;
+# if age<=13:
+#     print('you are teenager')
+# elif age<=20:
+#     print("you are adult")
+# elif age>20:
+#     print("you are old")
+# else:
+#     print("you are a child")
+
+
+
+
+
+
+def cal(a,b):
+    print('the sum of two number is',a+b);
+    print('the product of two number is',a*b);
+    print('the reminder of two number is',a-b);
+    print('the dividednt of two number is',a/b);
+cal(10,15)
